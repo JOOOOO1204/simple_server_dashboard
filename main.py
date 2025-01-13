da4559d0d2062f155809a6cfa794a0f3dea5a7d7
JOOOOO <111738796+JOOOOO1204@users.noreply.github.com>
2025-01-13 16:25:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import time
 import os
 
 
-PUBLIC_DIR = "D:\windows direcotry\Downloads\Banana.Launcher\web" 
-PORT = 8080  
+PUBLIC_DIR = "web" 
+PORT = 19000
 
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
@@ -46,29 +46,11 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         pass  
 
 
-def choose_language():
-    print("Escolha o idioma / Choose your language:")
-    print("1. Português")
-    print("2. English")
-    choice = input("Digite 1 ou 2: ")
-
-    if choice == '1':
-        print("Idioma definido como Português.")
-        return 'pt'
-    elif choice == '2':
-        print("Language set to English.")
-        return 'en'
-    else:
-        print("Opção inválida. Usando Português como padrão.")
-        return 'pt'
-
 
 if __name__ == "__main__":
     start_time = time.time() 
     os.chdir(PUBLIC_DIR)
 
-    
-    language = choose_language()
 
     print(f'Servidor iniciado em: http://127.0.0.1:{PORT}')
     print(f'Diretório público definido: {PUBLIC_DIR}')
